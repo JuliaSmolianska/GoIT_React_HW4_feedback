2dbdade9b2b927dd02194149e898768133a1aee1
JuliaSmolianska <i.julia.i1990@gmail.com>
2023-08-28 14:09:59 +0300

    added  icons

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,15 +1,28 @@
 import css from './Feedback.module.css';
+import {
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from 'react-icons/bs';
 import PropTypes from 'prop-types';
+
+const icons = [
+  <BsFillEmojiSmileFill />,
+  <BsFillEmojiNeutralFill />,
+  <BsFillEmojiFrownFill />,
+];
 
 export const Feedback = ({ options, onLeaveFeedback }) => {
   return (
     <div className={css.box}>
-      {options.map(option => (
+      {options.map((option, index) => (
         <button
           className={css.button_vote}
           key={option}
           onClick={() => onLeaveFeedback(option)}
         >
+          {' '}
+          <span>{icons[index]}</span>
           {option}
         </button>
       ))}
